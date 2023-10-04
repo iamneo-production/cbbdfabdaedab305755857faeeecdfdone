ccbbbf4b95e4df51bfed3a6981e5da45589a9fe5
Iamneo <techadmin@iamneo.ai>
2023-10-04 14:02:27 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/LaptopController.java
+++ springapp/src/main/java/com/examly/springapp/controller/LaptopController.java
@@ -2,3 +2,16 @@ package com.examly.springapp.controller;
 import com.examly.springapp.model.Laptop;
 import com.examly.springapp.service.*;
 
+
+@RequestMapping("api/v1")
+public class LaptopController{
+
+    @Autowired
+    private ApiService laptopService;
+    
+    @PostMapping("/")
+
+    @GetMapping("/laptop/{laptopId")
+
+    @GetMapping("")
+}
