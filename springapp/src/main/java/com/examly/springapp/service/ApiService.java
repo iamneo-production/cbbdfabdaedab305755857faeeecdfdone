233a12714b95cce37fbb7496d685797cc49ec522
Iamneo <techadmin@iamneo.ai>
2023-10-04 13:42:27 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/service/ApiService.java
+++ springapp/src/main/java/com/examly/springapp/service/ApiService.java
@@ -1,5 +1,14 @@
 package com.examly.springapp.service;
 
-import com.examly.springapp.model.Laptop;
+import com.examly.springapp.model.Laptop
 
-public class Laptop
+public class LaptopService{
+
+    public Laptop retrieveLaptop(Laptop laptop){
+        Laptop newLaptopId = laptop.getLaptopId();
+        Laptop newLaptopBrand = laptop.getLaptopBrand();
+        Laptop newLaptopPrice = laptop.getLaptopPrice();
+        return newLapto;
+    }
+
+}
